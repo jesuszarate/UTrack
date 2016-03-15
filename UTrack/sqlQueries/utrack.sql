6bf407df61dc66e345151fa8ab90d4ebf6a22ef1
Jesus <chuy8jay@gmail.com>
2016-03-15 17:37:12 -0600

    Added all of the tables to database

--- UTrack/sqlQueries/utrack.sql
+++ UTrack/sqlQueries/utrack.sql
@@ -10,11 +10,139 @@ PRIMARY KEY (login));
 INSERT INTO Users
 (login, name, userType, password, address, phone_num)
 VALUES
-('hello', 'helloworld', 'programmer', '222 south main', 'helloworld8', '(801)555-8460');
+('hello', 'helloworld', 'programmer', 'helloworld8', '222 south main', '(801)555-8460');
 
-
-
+-----------------------------------------------------------------------------------------------
 CREATE TABLE POI(
-pid int primary key, 
-name varchar[50], 
-category varchar[20]);
+pid INTEGER,
+name VARCHAR(50), 
+category VARCHAR(20), 
+PRIMARY KEY (pid));
+
+INSERT INTO POI
+(pid, name, category)
+VALUES
+(200, 'Jack Mormon', 'coffee shop');
+
+-----------------------------------------------------------------------------------------------
+
+CREATE TABLE VisEvent(
+vid INTEGER,
+cost INTEGER, 
+numberofheads INTEGER,
+PRIMARY KEY(vid));
+
+INSERT INTO VisEvent
+(vid, cost, numberofheads)
+VALUES
+(300, 20, 2);
+
+-----------------------------------------------------------------------------------------------
+
+CREATE TABLE Visit(
+login char(30),
+pid INTEGER,
+vid INTEGER,
+visitdate DATE,
+PRIMARY KEY (login, pid, vid),
+FOREIGN KEY (login) REFERENCES Users (login),
+FOREIGN KEY (pid) REFERENCES POI (pid),
+FOREIGN KEY (vid) REFERENCES VisEvent (vid));
+
+
+INSERT INTO Visit
+(login, pid, vid, visitdate)
+VALUES
+('chuy8jay', 200, 300, to_date(03142016));
+-----------------------------------------------------------------------------------------------
+
+CREATE TABLE Trust(
+login1 char(30),
+login2 char(30),
+isTrusted boolean,
+PRIMARY KEY (login1, login2),
+FOREIGN KEY (login1) REFERENCES Users (login),
+FOREIGN KEY (login2) REFERENCES Users (login));
+
+INSERT INTO Trust
+(login1, login2, isTrusted)
+VALUES
+('chuy8jay', 'jay8chuy', 1);
+
+-----------------------------------------------------------------------------------------------
+
+CREATE TABLE Rates(
+login CHAR(30),
+fid INTEGER,
+rating CHAR(20),
+PRIMARY KEY (login,fid),
+FOREIGN KEY (login) REFERENCES Users (login),
+FOREIGN KEY (fid) REFERENCES Feedback (fid));
+
+INSERT INTO Rates
+(login, fid, rating)
+VALUES
+('chuy8jay', 400, 'Very Good');
+
+-----------------------------------------------------------------------------------------------
+
+CREATE TABLE Feedback(
+fid INTEGER,
+pid INTEGER NOT NULL,
+login char(30) NOT NULL,
+text char(100),
+fbdate date,
+score INTEGER,
+PRIMARY KEY (fid),
+FOREIGN KEY (pid) REFERENCES POI (pid),
+FOREIGN KEY (login) REFERENCES Users (login));
+
+
+INSERT INTO Feedback
+(fid, pid, login, text, fbdate, score)
+VALUES
+(400, 200, 'chuy8jay', 'Best coffee ever','2016-14-03', 10);
+
+-----------------------------------------------------------------------------------------------
+
+CREATE TABLE Favorites(
+pid INTEGER NOT NULL,
+login char(30) NOT NULL,
+fvdate date,
+PRIMARY KEY (pid, login),
+FOREIGN KEY (pid) REFERENCES POI (pid),
+FOREIGN KEY (login) REFERENCES Users (login));
+
+INSERT INTO Favorites
+(pid, login, fvdate)
+VALUES
+(200, 'chuy8jay', '2016-14-03');
+
+-----------------------------------------------------------------------------------------------
+
+CREATE TABLE Keywords(
+wid INTEGER,
+word CHAR(50), 
+language CHAR(20),
+PRIMARY KEY(wid));
+
+INSERT INTO Keywords
+(wid, word, language)
+VALUES
+(500, 'coffee', 'English');
+
+-----------------------------------------------------------------------------------------------
+
+CREATE TABLE HasKeywords(
+pid INTEGER,
+wid INTEGER,
+PRIMARY KEY (pid, wid),
+FOREIGN KEY (pid) REFERENCES POI (pid),
+FOREIGN KEY (wid) REFERENCES Keywords (wid));
+
+INSERT INTO HasKeywords
+(pid, wid)
+VALUES
+(200, 500);
+
+
