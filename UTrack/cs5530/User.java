0d45519dbe0d384d89cafc78069a3643cfb9e7f9
Jesus <chuy8jay@gmail.com>
2016-03-15 22:42:37 -0600

    Successfully adding visits to a poi

--- UTrack/cs5530/User.java
+++ UTrack/cs5530/User.java
@@ -3,20 +3,17 @@ package cs5530;
 import java.sql.*;
 
 public class User {
+
+    String login;
+    String name;
+    
     public User()
     {}
-
-    public String registerUser(String login, String name,  boolean userType, String password, String address, String phone_num, Connection con, Statement stmt)
-    {
-	//String sql = "INSERT INTO Users (login, name, userType, password, address, phone_num) VALUES" +
-	//    "('%"+ login + "%','%" + name + "%','%" + password + "%','%" + userType  + "%','%" + address  + "%','%" + phone_num + "%')";
-
-	/*
-	userType = true;
-	String sql = "INSERT INTO Users (login, name, userType, password, address, phone_num) VALUES" +
-	    "('chuy8jay','jesus zarate','password',"+userType+",'222 s main','123-456-7890')";
-	*/
-	//"('%chuy8jay%','%jesus zarate%','%password%','%userType%','%222 s main%','%123-456-7890%')";
+    
+    public String registerUser(String _login, String _name,  boolean _userType, String _password, 
+			       String _address, String _phone_num, Connection con, Statement stmt){
+	login = _login;
+	name = _name;	
 
 	String sql = "INSERT INTO Users (login, name, userType, password, address, phone_num)" +
 	    "VALUES (?, ?, ?, ?, ?, ?)";
@@ -26,31 +23,19 @@ public class User {
 	System.out.println("executing " + sql);
 	try{       
 	    PreparedStatement preparedStatement = con.prepareStatement(sql);
-	    preparedStatement.setString(1, login);
-	    preparedStatement.setString(2, name);
-	    preparedStatement.setBoolean(3, userType);
-	    preparedStatement.setString(4, password);
-	    preparedStatement.setString(5, address);
-	    preparedStatement.setString(6, phone_num);
+	    preparedStatement.setString(1, _login);
+	    preparedStatement.setString(2, _name);
+	    preparedStatement.setBoolean(3, _userType);
+	    preparedStatement.setString(4, _password);
+	    preparedStatement.setString(5, _address);
+	    preparedStatement.setString(6, _phone_num);
 	    preparedStatement.executeUpdate();
-	
 	}
-	catch(Exception e)
-	    {
-		System.out.println(e.toString());
-		System.out.println("Cannot execute the query");
-	    }
-	finally
-	    {
-		try{
-		    //if (rs != null && !rs.isClosed())
-		    //	rs.close();
-		}
-		catch(Exception e)
-		    {
-			System.out.println("Cannot close resultset");
-		    }
-	    }
+	catch(Exception e){	    
+	    System.out.println(e.toString());
+	    System.out.println("Cannot execute the query");
+	}
+
 	return output;
     }
 
@@ -89,40 +74,115 @@ public class User {
 	return output;
     }
 
-    /*
-      public String getUser(String uname, String dname, Statement stmt)
-      {
-      String sql = "select * from course where cname like '%"+uname+"%' and dname like '%"+dname+"%'";
-      String output = "";
-      ResultSet rs = null;
-      System.out.println("executing "+sql);
-      try{
-      // Execute sql query
-      rs = stmt.executeQuery(sql);
 
-      while (rs.next())
-      {
-      output+=rs.getString("uname")+"   "+rs.getString("dname")+"\n"; 
-      }
-			     
-      rs.close();
-      }
-      catch(Exception e)
-      {
-      System.out.println("Cannot execute the query");
-      }
-      finally
-      {
-      try{
-      if (rs!=null && !rs.isClosed())
-      rs.close();
-      }
-      catch(Exception e)
-      {
-      System.out.println("Cannot close resultset");
-      }
-      }
-      return output;
-      }
-    */
+    public boolean loginUser(String login, String password, Statement stmt){
+    
+	String sql = "SELECT * " +
+	    "FROM Users " +
+	    "WHERE login = '" + login + "'";
+
+	String output = "";
+	ResultSet rs = null;
+	System.out.println("Executing: " + sql);
+	try{
+	    // Execute sql query
+	    rs = stmt.executeQuery(sql);
+
+	    String p;
+	    while (rs.next()){
+		login = rs.getString("login");
+		name = rs.getString("name");
+		p = rs.getString("password");
+		output += 
+		    "Username: " + login +  "\n" + 
+		    "Password: " + p + "\n";
+
+		if(password.equals(p))
+		    return true;
+	    }			     
+	    rs.close();
+	}
+	catch(Exception e){
+	    System.out.println(e.toString());
+	    System.out.println("Cannot execute the query");
+	}
+	finally{	 
+	    try{
+		if (rs!=null && !rs.isClosed())
+		    rs.close();
+	    }
+	    catch(Exception e){
+		System.out.println("Cannot close resultset");
+	    }
+	}
+	login = null;
+	password = null;
+	return false;
+    }
+    
+    public String addVisit(String _pname, int _cost, int _numberOfPeople, String _date,
+			   Statement stmt, Connection con){
+	int pid = -1;
+	int vid = 301; // Auto increment this number
+
+	
+	String sql = "SELECT pid FROM POI WHERE name LIKE '%" + _pname +"%'";
+	ResultSet rs = null;
+	try{       
+	    rs = stmt.executeQuery(sql);
+
+	    while (rs.next())
+		pid = rs.getInt("pid");
+	    
+	    rs.close();
+	}
+	catch(Exception e){	    
+	    System.out.println(e.toString());
+	    System.out.println("Cannot execute the query");
+	}
+	finally{
+	    try{
+		if (rs != null && !rs.isClosed())
+		    rs.close();
+	    }
+	    catch(Exception e){
+		System.out.println("cannot close resultset");
+	    }
+	}
+
+
+	String output = "";
+	try{       
+	    sql = "INSERT INTO VisEvent (vid, cost, numberofheads)" +
+	    "VALUES (?, ?, ?)";
+
+	    PreparedStatement preparedStatement = con.prepareStatement(sql);
+	    preparedStatement.setInt(1, vid);
+	    preparedStatement.setInt(2, _cost);
+	    preparedStatement.setInt(3, _numberOfPeople);
+
+	    preparedStatement.executeUpdate();
+
+	    sql = "INSERT INTO Visit (login, pid, vid, visitdate)" +
+	    "VALUES (?, ?, ?, ?)";
+
+	    preparedStatement = con.prepareStatement(sql);
+	    preparedStatement.setString(1, login);
+	    preparedStatement.setInt(2, pid);
+	    preparedStatement.setInt(3, vid);
+	    // Todo: Change this to _date
+	    preparedStatement.setDate(4, java.sql.Date.valueOf("2013-09-04"));
+
+	    preparedStatement.executeUpdate();
+	}
+	catch(Exception e){	    
+	    System.out.println(e.toString());
+	    System.out.println("Cannot execute the query 2");
+	}
+	
+	return output;
+
+    }
+    
+
 }
--- UTrack/cs5530/testdriver2.java
+++ UTrack/cs5530/testdriver2.java
@@ -7,138 +7,184 @@ import java.io.*;
 
 public class testdriver2 {
 
-	/**
-	 * @param args
-	 */
-	public static void displayMenu()
-	{
-	    System.out.println("        Welcome to the UTrack System     ");
-	    System.out.println("1. search a course by cname and dname:");
-	    System.out.println("2. Add a user:");
-	    System.out.println("3. Get Users:");
-	    System.out.println("4. enter your own query:");
-	    System.out.println("5. exit:");
-	    System.out.println("pleasse enter your choice:");
+    static String name;
+    static String login = null;
+    static String password;
+    static String address;
+    static String phone_num;
+    static Connector con = null;
+    static String choice;
+    static String sql = null;
+    static int c = 0;
+    static User user;
+    static boolean logedIn = false;
+
+    /**
+     * @param args
+     */
+    public static void displayMenu()
+    {
+	System.out.println("        Welcome to the UTrack System     ");
+	if(!logedIn){
+	    System.out.println("1. Login: ");
+	    System.out.println("2. Register user:");
 	}
+	System.out.println("3. Add Visit to POI:");
+	System.out.println("4. enter your own query:");
+	System.out.println("5. exit:");
+	System.out.println("please enter your choice:");
+    }
 	
-	public static void main(String[] args) {
-	    // TODO Auto-generated method stub
-	    System.out.println("Example for cs5530");
-	    Connector con = null;
-	    String choice;
-	    String cname;
-	    String dname;
-	    String sql = null;
-	    int c = 0;
-	    try
-		{
-		    //remember to replace the password
-		    con = new Connector();
-		    System.out.println ("Database connection established");
+    public static void main(String[] args) {
+	user = new User();
+
+	System.out.println("Example for cs5530");
+
+	try{	    
+	    //remember to replace the password
+	    con = new Connector();
+	    System.out.println ("Database connection established");
 		    
-		    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 	            
-		    while(true)
-			{
-			    displayMenu();
-			    while ((choice = in.readLine()) == null && choice.length() == 0);
-			    try{
-				c = Integer.parseInt(choice);
-			    }catch (Exception e)
-				{
-	            		 
-				    continue;
-				}
-			    if (c<1 | c>5)
-				continue;
-			    if (c == 2)
-				{
-				    String name;
-				    String login;
-				    String password;
-				    String address;
-				    String phone_num;
-
-				    System.out.println("Enter your user login:");
-				    while ((login = in.readLine()) == null && login.length() == 0);
-				    
-				    System.out.println("Enter your full name:");
-				    while ((name = in.readLine()) == null && name.length() == 0);
-
-				    System.out.println("Enter your password:");
-				    while ((password = in.readLine()) == null && password.length() == 0);
-
-				    System.out.println("Enter your address:");
-				    while ((address = in.readLine()) == null && address.length() == 0);
-
-				    System.out.println("Enter your phone number:");
-				    while ((phone_num = in.readLine()) == null && phone_num.length() == 0);
-				    
-				    User user = new User();
-				    System.out.println(user.registerUser(login, name, true, password, address, phone_num, con._con, con.stmt));
-				}
-			    if (c==1)
-				{
-				    System.out.println("please enter a cname:");
-				    while ((cname = in.readLine()) == null && cname.length() == 0);
-				    System.out.println("please enter a dname:");
-				    while ((dname = in.readLine()) == null && dname.length() == 0);
-				    Course course=new Course();
-				    System.out.println(course.getCourse(cname, dname, con.stmt));
-				}
-			    if (c==3)
-				{
-				    
-				    User user = new User();
-				    System.out.println(user.registerUser("", "", true,"", "", "", con._con, con.stmt));
-
-				    //User user = new User();
-				    //System.out.println(user.getUsers(con.stmt));
-				}
-			    else if (c == 4)
-				{	 
-				    System.out.println("please enter your query below:");
-				    while ((sql = in.readLine()) == null && sql.length() == 0)
-					System.out.println(sql);
-				    ResultSet rs=con.stmt.executeQuery(sql);
-				    ResultSetMetaData rsmd = rs.getMetaData();
-				    int numCols = rsmd.getColumnCount();
-				    while (rs.next())
-					{
-					    //System.out.print("cname:");
-					    for (int i=1; i<=numCols;i++)
-						System.out.print(rs.getString(i)+"  ");
-					    System.out.println("");
-					}
-				    System.out.println(" ");
-				    rs.close();
-				}
-			    else
-				{   
-				    System.out.println("Remeber to pay us!");
-				    con.stmt.close(); 
-				    
-				    break;
-				}
-			}
-		}
-	    catch (Exception e)
+	    while(true)
 		{
-		    e.printStackTrace();
-		    System.err.println ("Either connection error or query execution error!");
-		}
-	    finally
-		{
-		    if (con != null)
-			{
-			    try
-				{
-				    con.closeConnection();
-				    System.out.println ("Database connection terminated");
-				}
-			 
-			    catch (Exception e) { /* ignore close errors */ }
-			}	 
+		    displayMenu();
+		    while ((choice = in.readLine()) == null && choice.length() == 0);
+		    try{
+			c = Integer.parseInt(choice);
+		    }catch (Exception e){	            		 
+			continue;
+		    }
+		    
+		    if (c == 0){
+			user.login = "jay8chuy";
+			user.addVisit("Jack Mormon", 20, 2, "12345", con.stmt, con._con);
+		    }
+		    
+		    if (c<1 | c>5)
+			continue;
+
+		    switch (c){
+		    case 1: // Login
+			login(in);
+			break;
+		    case 2: // Register
+			registerUser(in);
+			break;
+		    case 3: // Add visit
+			if(logedIn)
+			    addVisit(in);
+			else
+			    System.out.println(
+					       "You are not logged in. Log in and try again.");
+			break;
+		    case 4:
+			break;
+		    default:
+			System.out.println("Remeber to pay us!");
+			con.stmt.close(); 				    
+			break;
+		    }
 		}
 	}
+	catch (Exception e){
+	    e.printStackTrace();
+	    System.err.println ("Either connection error or query execution error!");
+	}
+	finally{
+	    if (con != null){
+		try{
+		    con.closeConnection();
+		    System.out.println ("Database connection terminated");
+		}
+			 
+		catch (Exception e) { /* ignore close errors */ }
+	    }	 
+	}
+    }
+
+    public static void registerUser(BufferedReader in) throws IOException {
+	System.out.println("Enter your user login:");
+	while ((login = in.readLine()) == null && login.length() == 0);
+				    
+	System.out.println("Enter your full name:");
+	while ((name = in.readLine()) == null && name.length() == 0);
+
+	System.out.println("Enter your password:");
+	while ((password = in.readLine()) == null && password.length() == 0);
+
+	System.out.println("Enter your address:");
+	while ((address = in.readLine()) == null && address.length() == 0);
+
+	System.out.println("Enter your phone number:");
+	while ((phone_num = in.readLine()) == null && phone_num.length() == 0);
+				           
+	System.out.println(
+			   user.registerUser(login, name, true, password, address, 
+					     phone_num, con._con, con.stmt));
+    }
+
+    public static void login(BufferedReader in) throws IOException {
+
+	System.out.println("Enter your user login:");
+	while ((login = in.readLine()) == null && login.length() == 0);
+	
+	System.out.println("Enter your password:");
+	while ((password = in.readLine()) == null && password.length() == 0);
+
+	if (user.loginUser(login, password, con.stmt)){
+	    logedIn = true;
+	    System.out.println("Login Successfull!");	    
+	}
+	else{
+	    login = null;
+	    password = null;
+	    System.out.println("Invalid password or username");
+	}
+    }
+
+    public static void addVisit(BufferedReader in) throws IOException{
+	POI poi = new POI();
+	String pname;
+	String date;
+	String answer;
+	String scost;
+	String snumOfPeople;
+
+	System.out.println("\n" + poi.getPOIs(con.stmt));
+	
+	System.out.println("Which POI did you visit:");
+	while ((pname = in.readLine()) == null && pname.length() == 0);	
+
+	System.out.println("What was the cost of the visit: ");
+	while ((scost = in.readLine()) == null && scost.length() == 0);	
+
+	System.out.println("How many people attended: ");
+	while ((snumOfPeople = in.readLine()) == null && snumOfPeople.length() == 0);
+
+	System.out.println("What was the date of the visit\n"+
+			   "add in this format (YYYYDDMM):");
+	while ((date = in.readLine()) == null && date.length() == 0);	
+
+	System.out.println("You visited " + pname + " on " + date + 
+			   "\nAre you sure you want to add this to your visits?" + 
+			   "\nType yes/no");
+	while ((answer = in.readLine()) == null && answer.length() == 0);	
+
+	int cost;
+	int numOfPeople;
+	try{
+	    cost = Integer.parseInt(scost);
+	    numOfPeople = Integer.parseInt(snumOfPeople);
+	}
+	catch(Exception e){
+	    System.out.println("Make sure the cost and the number of people is a number");
+	    return;
+	}
+	answer = answer.toLowerCase();
+	if (answer.equals("yes")){
+	    user.addVisit(pname, cost, numOfPeople, date, con.stmt, con._con);
+	}
+    }
 }
