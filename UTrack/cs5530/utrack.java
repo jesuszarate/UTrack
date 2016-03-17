d2f4b01a90194acdbf291078c5f5697afc54d0d6
Jesus <chuy8jay@gmail.com>
2016-03-16 19:59:11 -0600

    Able to update pois

--- UTrack/cs5530/utrack.java
+++ UTrack/cs5530/utrack.java
@@ -29,8 +29,9 @@ public class utrack {
 	    System.out.println("2. Register user:");
 	}
 	System.out.println("3. Add Visit to POI:");
-	System.out.println("4. enter your own query:");
-	System.out.println("5. exit:");
+	System.out.println("4. Add new POI:");
+	System.out.println("5. Update POI:");
+	System.out.println("6. exit:");
 	System.out.println("please enter your choice:");
     }
 	
@@ -57,8 +58,12 @@ public class utrack {
 		    }
 		    
 		    if (c == 0){
+			POI poi =  new POI();
 			user.login = "jay8chuy";
-			user.addVisit("Jack Mormon", 20, 2, "12345", con.stmt, con._con);
+			poi.addPOI("Tony's", "Restaurant", "222 s main", "tonys.com", 
+				   "(801)123-45678", 1990, "10am-3am", 10,
+				   con._con, con.stmt);
+				   
 		    }
 		    
 		    if (c<1 | c>5)
@@ -79,7 +84,14 @@ public class utrack {
 					       "You are not logged in. Log in and try again.");
 			break;
 		    case 4:
-			break;
+			if(logedIn)
+			    addPOI(in);
+			else
+			    System.out.println(
+					       "You are not logged in. Log in and try again.");
+			    break;
+		    case 5:
+			updatePOI(in);
 		    default:
 			System.out.println("Remeber to pay us!");
 			con.stmt.close(); 				    
@@ -141,6 +153,143 @@ public class utrack {
 	    password = null;
 	    System.out.println("Invalid password or username");
 	}
+    }
+
+    public static void updatePOI(BufferedReader in) throws IOException{
+	String choice;
+	String setQuery = "";
+	String poiName;
+	System.out.println("Which POI would you like to modify:");
+	while ((choice = in.readLine()) == null && choice.length() == 0);
+	poiName = choice;
+	
+	boolean incomingInput = true;
+	while(incomingInput){
+	    int c = 0;
+	    System.out.println("Enter 0 when done");
+	    System.out.println("1: name");
+	    System.out.println("2, category");
+	    System.out.println("3, address");
+	    System.out.println("4, URL");
+	    System.out.println("5, tel_num");
+	    System.out.println("6, yr_est");
+	    System.out.println("7, hours");
+	    System.out.println("8, price");
+
+	    System.out.println("Which field woud you like to modify:");
+	    while ((choice = in.readLine()) == null && choice.length() == 0);
+
+	    try{
+		c = Integer.parseInt(choice);
+	    }
+	    catch(Exception e){
+		System.out.println("Must be a choice within range");
+		continue;
+	    }
+
+	    switch(c){
+	    case 0:
+		POI poi = new POI();
+		setQuery = setQuery.substring(0, setQuery.length() - 2);
+		System.out.println("\n" + setQuery + "\n");
+		poi.updatePOI(poiName, setQuery, con._con, con.stmt);
+		incomingInput = false;
+		break;
+	    case 1:
+		System.out.println("Enter new name:");
+		while ((choice = in.readLine()) == null && choice.length() == 0);
+		setQuery += "name = '" + choice + "', ";
+		break;
+	    case 2:
+		System.out.println("Enter new category:");
+		while ((choice = in.readLine()) == null && choice.length() == 0);
+		setQuery += "category = '" + choice + "', ";
+		break;
+	    case 3:
+		System.out.println("Enter new address:");
+		while ((choice = in.readLine()) == null && choice.length() == 0);
+		setQuery += "address = '" + choice + "', ";
+		break;
+	    case 4:
+		System.out.println("Enter new URL:");
+		while ((choice = in.readLine()) == null && choice.length() == 0);
+		setQuery += "URL = '" + choice + "', ";
+		break;
+	    case 5:
+		System.out.println("Enter new phone number:");
+		while ((choice = in.readLine()) == null && choice.length() == 0);
+		setQuery += "tel_num = '" + choice + "', ";
+		break;
+	    case 6:
+		System.out.println("Enter new year of establishment:");
+		while ((choice = in.readLine()) == null && choice.length() == 0);
+		setQuery += "yr_est = '" + choice + "', ";
+		break;
+	    case 7:
+		System.out.println("Enter new hours of business:");
+		while ((choice = in.readLine()) == null && choice.length() == 0);
+		setQuery += "hours = '" + choice + "' ";
+		break;
+	    case 8:
+		System.out.println("Enter new price:");
+		while ((choice = in.readLine()) == null && choice.length() == 0);
+		setQuery += "price = '" + choice + "', ";
+		break;
+	    default:
+		System.out.println("No available choice");
+	    }
+	}
+
+    }
+
+    public static void addPOI(BufferedReader in) throws IOException{	
+	POI poi = new POI();
+	String name;
+	String category;
+	String address;
+	String URL;
+	String tel_num;
+	int yr_est;
+	String hours;
+	int price;
+
+	try{
+	    System.out.println("What is the name of the POI:");
+	    while ((name = in.readLine()) == null && name.length() == 0);
+	
+	    System.out.println("What category does it fall in:");
+	    while ((category = in.readLine()) == null && category.length() == 0);
+
+	    System.out.println("What is the POI's address:");
+	    while ((address = in.readLine()) == null && address.length() == 0);
+
+	    System.out.println("What is the POI's URL:");
+	    while ((URL = in.readLine()) == null && URL.length() == 0);
+
+	    System.out.println("What is the POI's phone number:");
+	    while ((tel_num = in.readLine()) == null && tel_num.length() == 0);
+	    
+	    String yr;
+	    System.out.println("What is the POI's year of establishment:");
+	    while ((yr = in.readLine()) == null && yr.length() == 0);
+	    
+	    System.out.println("What are the hours of operation:");
+	    while ((hours = in.readLine()) == null && hours.length() == 0);
+
+	    String pr;
+	    System.out.println("What is the estimated price per person:");
+	    while ((pr = in.readLine()) == null && pr.length() == 0);
+	    
+	    yr_est = Integer.parseInt(yr);
+	    price = Integer.parseInt(pr);	    
+
+	    poi.addPOI(name, category, address, URL, tel_num, yr_est, hours, price,
+		       con._con, con.stmt);
+	}
+	catch (Exception e){
+	    System.out.println("Must provide a number");
+	}       
+	
     }
 
     public static void addVisit(BufferedReader in) throws IOException{
